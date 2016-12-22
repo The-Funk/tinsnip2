8988bac36dc2084290c9665c73edfe58a2a2952d
cole <icsnyder42@gmail.com>
2016-12-21 20:38:35 -0500

    Updated main to take command line args

--- main.cpp
+++ main.cpp
@@ -2,9 +2,25 @@
 #include "tins/tins.h"
 //#include "zdb/zdb.h"
 
+using namespace Tins;
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
+int main ( int argc, char *argv[] ) {
+
+    if (argc < 2) {
+        cout << "No args" << endl;
+    }
+    else {
+        cout << "You right" << endl;
+    }
+
+    //EthernetII eth;
+    //IP *ip = new IP();
+    //TCP *tcp = new TCP();
+    // tcp is ip's inner pdu
+    //ip->inner_pdu(tcp);
+    // ip is eth's inner pdu
+    //eth.inner_pdu(ip);
+
     return 0;
 }
