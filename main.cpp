5adb9b0dbc31102a85819720e18436108fe42617
cole <icsnyder42@gmail.com>
2017-05-31 00:11:41 -0400

    Removed old option parser and replaced with TCLAP.

--- main.cpp
+++ main.cpp
@@ -2,101 +2,40 @@
 #include "zdb/zdb.h"
 #include <string>
 #include <vector>
-#include "optionparser.h"
+#include <tclap/CmdLine.h>
 
 using namespace std;
+using namespace TCLAP;
 
-struct Arg: public option::Arg
-{
-    static void printError(const char* msg1, const option::Option& opt, const char* msg2)
-    {
-        fprintf(stderr, "%s", msg1);
-        fwrite(opt.name, opt.namelen, 1, stderr);
-        fprintf(stderr, "%s", msg2);
-    }
+int main(int argc, char* argv[]) {
+    try {
+        //Create argument parser
+        CmdLine argp("For more help, you can find examples on the web at www.foo.bar", ' ', "0.1");
 
-    static option::ArgStatus Unknown(const option::Option& option, bool msg)
-    {
-        if (msg) printError("Unknown option '", option, "'\n");
-        return option::ARG_ILLEGAL;
-    }
+        //Add Value storing Arguments....
 
-    static option::ArgStatus Required(const option::Option& option, bool msg)
-    {
-        if (option.arg != 0)
-            return option::ARG_OK;
+        //Source IP Arg
+        ValueArg<string> srcArg("s","src-ip","IP Address where traffic originates",true,"192.168.x.x","");
+        //Source IP Range Arg
+        ValueArg<string> rngArg("r","src-range","IP Range where traffic originates",true,"192.168.x.x","");
+        //Host IP Arg
+        ValueArg<std::string> hostArg("h","host-ip","Address of host machine",true,"192.168.x.x","");
 
-        if (msg) printError("Option '", option, "' requires an argument\n");
-        return option::ARG_ILLEGAL;
-    }
+        //Add Switch Arguments....
 
-    static option::ArgStatus NonEmpty(const option::Option& option, bool msg)
-    {
-        if (option.arg != 0 && option.arg[0] != 0)
-            return option::ARG_OK;
+        //Set sniffer on/off, default value runs program with sniffer off
+        SwitchArg sniffArg("s","sniff-traffic","Print name backwards", argp, false);
+        //Set ARP poisoning on/off, default value runs program with Poisoning off
+        SwitchArg arpsnArg("p","poison-arp","Poison target machine's ARP cache", argp, false);
+        
+        argp.parse(argc,argv);
 
-        if (msg) printError("Option '", option, "' requires a non-empty argument\n");
-        return option::ARG_ILLEGAL;
-    }
-
-    static option::ArgStatus Numeric(const option::Option& option, bool msg)
-    {
-        char* endptr = 0;
-        if (option.arg != 0 && strtol(option.arg, &endptr, 10)){};
-        if (endptr != option.arg && *endptr == 0)
-            return option::ARG_OK;
-
-        if (msg) printError("Option '", option, "' requires a numeric argument\n");
-        return option::ARG_ILLEGAL;
-    }
-};
-
-enum  optionIndex { UNKNOWN, HELP, GWAY, TRGT, SNF, PSN };
-const option::Descriptor usage[] =
-        {
-                {UNKNOWN, 0, "", "",Arg::None, "USAGE: example [options]\n\n"
-                                                               "Options:" },
-                {HELP, 0, "h", "help",Arg::None, "  --help  \tPrint usage and exit." },
-                {GWAY, 1, "g", "gateway",Arg::Required, "  --gateway, -g  \tSet the gateway address." },
-                {TRGT, 2, "t", "target",Arg::Optional, "  --target, -t  \tSet the target machine address." },
-                {SNF, 3, "s", "sniff",Arg::None, "  --sniff, -s  \tSet program to sniff traffic only." },
-                {PSN, 3, "p", "poison",Arg::None, "  --poison, -p  \tSet program to perform ARP poisoning." },
-                {0,0,0,0,0,0}
-        };
-
-int main(int argc, char* argv[])
-{
-    argc-=(argc>0); argv+=(argc>0); // skip program name argv[0] if present
-    option::Stats  stats(usage, argc, argv);
-    vector<option::Option> options(stats.options_max);
-    vector<option::Option> buffer(stats.buffer_max);
-    option::Parser parse(usage, argc, argv, &options[0], &buffer[0]);
-
-    for (int i = 0; i < parse.optionsCount(); ++i)
-    {
-        option::Option& opt = buffer[i];
-        switch (opt.index())
-        {
-            case HELP:
-                option::printUsage(cout, usage);
-                break;
-            case GWAY:
-                if (opt.arg)
-                    cout << "Found arg" << opt.arg << endl;
-                else
-                    cout << "Argument not found." << endl;
-                break;
-            case UNKNOWN:
-                // not possible because Arg::Unknown returns ARG_ILLEGAL
-                // which aborts the parse with an error
-                break;
+        if (sniffArg.getValue()){
+            cout << "Yep! It's parsin!" << endl;
         }
     }
-    if (parse.error())
-        return 1;
-
-    for (option::Option* opt = options[UNKNOWN]; opt; opt = opt->next())
-        std::cout << "Unknown option: " << std::string(opt->name,opt->namelen) << "\n";
-
+    catch (TCLAP::ArgException &e) {
+        cerr << "Error: " << e.error() << " in Arg: " << e.argId() << endl;
+    }
     return 0;
 }
--- sniffer.cpp
+++ sniffer.cpp
@@ -9,5 +9,13 @@ using namespace Tins;
 using namespace std;
 
 EthernetII eth = EthernetII() / IP() / TCP();
+
+//Create pointer for TCP frame PDU inside IP frame PDU inside Ethernet frame PDU
 TCP *tcp = eth.find_pdu<TCP>();
+
+//Create pointer for IP packet PDU inside Ethernet frame PDU
 IP *ip = eth.find_pdu<IP>();
+
+//Creates an IPV4 address
+IPv4Address lo("127.0.0.1");
+
